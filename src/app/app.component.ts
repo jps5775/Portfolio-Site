35d15eeff18d4d3f0cc0a56ce2bc5725e4358a0c
jps5775 <doej4566@gmail.com>
2019-06-23 20:11:27 -0400

    Added new functionality and designs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-custom-website';
+
+  
+  displayAboutMe(){
+    var aboutMeTag = document.getElementsByClassName("example-section")[0];
+    aboutMeTag.scrollIntoView();
+  }
+  
+  displayResume(){
+    var resumeTag = document.getElementsByClassName("example-section")[1];
+    resumeTag.scrollIntoView();
+  }
+
+  displayProjects(){
+    var projectsTag = document.getElementsByClassName("example-section")[2];
+    projectsTag.scrollIntoView();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRippleModule} from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatChipsModule} from '@angular/material/chips';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatCardModule,
+    MatGridListModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
